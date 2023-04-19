145ad7f3bf5b50aebca8be007a3fa16d7a6095d0
leo <vert.stylo@laposte.net>
2023-04-20 01:03:51 +0200

    added camera colorspace

--- main.py
+++ main.py
@@ -30,53 +30,88 @@ class App(customtkinter.CTk):
     super().__init__()
 
     # configure window
-    self.title("CustomTkinter complex_example.py")
-    self.geometry(f"{1100}x{580}")
+    self.title("ASL Recognition")
+    self.geometry(f"{1300}x{650}")
 
     # configure grid layout (4x4)
     self.grid_columnconfigure(1, weight=1)
-    self.grid_columnconfigure((2, 3), weight=0)
-    self.grid_rowconfigure((0, 1, 2), weight=1)
+    self.grid_columnconfigure((1, 3), weight=0)
+    self.grid_rowconfigure((0, 1, 2), weight=0)
 
-    # create sidebar frame with widgets
+    ## LEFT SIDEBAR
     self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
     self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
     self.sidebar_frame.grid_rowconfigure(4, weight=1)
-
-    self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
+    
+      # title
+    self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="YOLOv8 ASL detection", font=customtkinter.CTkFont(size=20, weight="bold"))
     self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+      # textbox
+    self.textbox = customtkinter.CTkTextbox(self.sidebar_frame, width=250)
+    self.textbox.grid(row=1, column=0, padx=20, pady=0)
+    self.textbox.insert("0.0", "This program uses YOLOv8 (Objet detection AI) to detect alphabet and numbers in the American Sign Language\n\n" +
+      "The base program was trained on a dataset of x images per class, y epochs,  z hyperparameters")
 
 
-    self.tabview = customtkinter.CTkTabview(self, width=250)
-    self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
+
+    ## CAMERA SETTINGS
+    self.camera_settings_panel = customtkinter.CTkFrame(self)
+    self.camera_settings_panel.grid(row=0, column=1, padx=(20, 20), pady=(20, 0), sticky="nsew")
+    
+    self.label_camera_settings_panel = customtkinter.CTkLabel(master=self.camera_settings_panel, text="Camera Settings", font=customtkinter.CTkFont(size=17, weight="bold"))
+    self.label_camera_settings_panel.grid(row=0, column=0, columnspan=1, padx=10, pady=10)
+
+      # camera output
+    self.list_camera_index = customtkinter.CTkOptionMenu(self.camera_settings_panel, dynamic_resizing=False, command=self.open_camera)
+    self.list_camera_index.grid(row=1, column=0, padx=20, pady=10, sticky="w")
+      # close camera
+    self.button_close_camera = customtkinter.CTkButton(self.camera_settings_panel, command=self.close_camera, text="Close camera")
+    self.button_close_camera.grid(row=2, column=0, padx=20, pady=10, sticky="w")
+    
+      # colorspace
+    self.camera_colorspace = customtkinter.CTkFrame(self.camera_settings_panel)
+    self.camera_colorspace.grid(row=3, column=0, padx=20, pady=10, sticky="w")
+    self.colorspace_var = tkinter.IntVar(value=0)
+    self.label_radio_group = customtkinter.CTkLabel(master=self.camera_colorspace, text="Camera Colorpace:")
+    self.label_radio_group.grid(row=0, column=0, columnspan=1, padx=20, pady=10, sticky="")
+        # RGB
+    self.camera_RGB = customtkinter.CTkRadioButton(master=self.camera_colorspace, text="RGB", variable=self.colorspace_var, value=0)
+    self.camera_RGB.grid(row=1, column=0, padx=20, pady=10, sticky="n")
+        # grayscale
+    self.camera_grayscale = customtkinter.CTkRadioButton(master=self.camera_colorspace, text="Grayscale", variable=self.colorspace_var, value=1)
+    self.camera_grayscale.grid(row=2, column=0, padx=20, pady=10, sticky="n")
+        # HSV
+    self.camera_HSV = customtkinter.CTkRadioButton(master=self.camera_colorspace, text="HSV", variable=self.colorspace_var, value=2)
+    self.camera_HSV.grid(row=3, column=0, padx=20, pady=10, sticky="n")
+
+
+
+    ## VIDEO PANEL
+    self.video_feed_panel = customtkinter.CTkFrame(self)
+    self.video_feed_panel.grid(row=0, column=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
+
+    self.label_video_feed = customtkinter.CTkLabel(master=self.video_feed_panel, text="Video output", font=customtkinter.CTkFont(size=17, weight="bold"))
+    self.label_video_feed.grid(row=0, column=0, columnspan=1, padx=10, pady=10)
+
+      # toggle YOLO
+    self.capture_on = customtkinter.IntVar(value=0)
+    self.switch_predict = customtkinter.CTkSwitch(master=self.video_feed_panel, text=f"Start YOLO", command = self.toggle_predict, variable=self.capture_on, onvalue=1, offvalue=0, state="disabled")
+    self.switch_predict.grid(row=1, column=0, padx=20, pady=5)
+      # video feed
+    self.tabview = customtkinter.CTkTabview(self.video_feed_panel, width=250, state="disabled")
+    self.tabview.grid(row=2, column=0, padx=0, pady=0, sticky="w")
     self.tabview.add("Video")
     self.source_image = tkinter.Label(self.tabview.tab("Video"))
     self.source_image.grid(row=0, column=0)
     self.predict_image = tkinter.Label(self.tabview.tab("Video"))
 
-    #OLD TABS FOR 'SOURCE' AND 'PREDICT'
-    """ # create tabview
-    self.tabview = customtkinter.CTkTabview(self, width=250)
-    self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-    self.tabview.add("Source")
-    self.tabview.add("Predict")
-    self.tabview.tab("Predict").grid_columnconfigure(0, weight=1)
 
-    # Create source & predict video feed
-    self.source_image = tkinter.Label(self.tabview.tab("Source"), text="source")
-    self.source_image.pack()
-    self.predict_image = tkinter.Label(self.tabview.tab("Predict"), text="predict")
-    self.predict_image.pack() """
-
-    self.capture_on = customtkinter.IntVar(value=0)
-    self.switch_predict = customtkinter.CTkSwitch(master=self, text=f"Toggle prediction", command = self.toggle_predict, variable=self.capture_on, onvalue=1, offvalue=0)
-    self.switch_predict.grid(row=0, column=0, padx=10, pady=(0, 20))
 
     self.load_model()
-    self.open_camera()
+    self.get_camera_indexes()
+
 
   def toggle_predict(self):
-
     if self.switch_predict.get():
       self.capture_on = True
       print("[-] Starting predictions...")
@@ -85,6 +120,7 @@ class App(customtkinter.CTk):
     else:
       self.capture_on = False
       self.predict_image.grid_forget()
+
 
   def load_model(self):
     self.model = YOLO('best.pt')
@@ -101,31 +137,64 @@ class App(customtkinter.CTk):
     print("[-] YOLOv8 loaded")
 
 
-  def open_camera(self):
-    self.cap = cv2.VideoCapture(0)
+  def get_camera_indexes(self):
+    # checks the first 10 indexes for devices
+    index = 0
+    self.arr = []
+    i = 10
+    while i > 0:
+        cap = cv2.VideoCapture(index)
+        if cap.read()[0]:
+            self.arr.append(str(index))
+            cap.release()
+        index += 1
+        i -= 1
+    self.list_camera_index.configure(values = self.arr)
+    self.list_camera_index.set("Select camera")
+
+  
+  def close_camera(self):
+    # stop capture and clean up menu + image
+    self.cap.release()
+    self.switch_predict.deselect()
+    self.switch_predict.configure(state="disabled")
+    self.predict_image.configure(image='')
+    self.source_image.configure(image='')
+    self.get_camera_indexes()
+
+
+  def open_camera(self, index):
+    # open camera, removes index from dropdown menu to avoid user capturing twice the same camera
+    self.cap = cv2.VideoCapture(int(index))
+    self.switch_predict.configure(state="enabled")
+    print("[-] Camera opened")
+    self.arr.remove(index)
+    self.list_camera_index.configure(values = self.arr)
     
-    if not self.cap.isOpened():
-      print("[X] Error while opening camera\nExiting...")
-      exit()
-    else:
-      print("[-] Camera opened")
-      self.get_source_feed()
+    self.get_source_feed()
+
 
   def get_source_feed(self):
     ret, frame = self.cap.read()
     
-    if not ret:
-      print("Not receiving frames (stream ended ?)\nExiting...")
-      exit()
+    if ret: # grab image + detect colorspace option
+      match self.colorspace_var.get():
+        case 0:
+          cv2image = cv2.cvtColor(frame ,cv2.COLOR_BGR2RGB)
+        case 1:
+          cv2image = cv2.cvtColor(frame ,cv2.COLOR_BGR2GRAY)
+        case 2:
+          cv2image = cv2.cvtColor(frame ,cv2.COLOR_BGR2HSV)
 
-    cv2image = cv2.cvtColor(frame ,cv2.COLOR_BGR2RGB) #grab image + colorspace
+      img = Image.fromarray(cv2image)
+      imgtk = ImageTk.PhotoImage(image = img)
+      self.source_image.imgtk = imgtk
+      self.source_image.configure(image=imgtk)
+      
+      self.source_image.after(10, self.get_source_feed)
+    else:
+      print("[-] Video stream ended")
 
-    img = Image.fromarray(cv2image)
-    imgtk = ImageTk.PhotoImage(image = img)
-    self.source_image.imgtk = imgtk
-    self.source_image.configure(image=imgtk)
-
-    self.source_image.after(20, self.get_source_feed)
 
 
   def get_predict_feed(self):
@@ -133,18 +202,28 @@ class App(customtkinter.CTk):
       ret, frame = self.cap.read()
 
       result_frame = self.predict(frame)
-    
-      cv2image = cv2.cvtColor(result_frame ,cv2.COLOR_BGR2RGB) #grab image + colorspace
+      
+      if ret: # grab image + detect colorspace option
+        match self.colorspace_var.get():
+          case 0:
+            cv2image = cv2.cvtColor(result_frame ,cv2.COLOR_BGR2RGB)
+          case 1:
+            cv2image = cv2.cvtColor(result_frame ,cv2.COLOR_BGR2GRAY)
+          case 2:
+            cv2image = cv2.cvtColor(result_frame ,cv2.COLOR_BGR2HSV)
 
-      img = Image.fromarray(cv2image)
-      imgtk = ImageTk.PhotoImage(image = img)
-      self.predict_image.imgtk = imgtk
-      self.predict_image.configure(image=imgtk)
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image = img)
+        self.predict_image.imgtk = imgtk
+        self.predict_image.configure(image=imgtk)
 
-      self.predict_image.after(20, self.get_predict_feed)
+        self.predict_image.after(20, self.get_predict_feed)
+      else:
+        print("[-] Video stream ended")
     else:
       self.predict_image.configure(image='')
       print("[-] Stopping predictions")
+
 
   def predict(self, frame):
 
@@ -188,6 +267,7 @@ class App(customtkinter.CTk):
       )
 
     return frame
+
 
 if __name__ == "__main__":
   app = App()
